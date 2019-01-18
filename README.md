c0be1e5ccf152bdf68a76d373710c6a8d9391ac1
improvshark <improvshark@gmail.com>
2019-01-17 23:30:38 -0700

    added skeletion, and refactored to use a deck class.

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Given a list of cards, this tool will make an image that can be used for tabletop
+
+[demo](https://improvshark.github.io/magic_to_tabletop/)
--- script.js
+++ script.js
@@ -1,123 +1,133 @@
 const CARD_SIZE = 'small'
 const API_DELAY = 50
 
-function loadImage(url) {
-  console.log('loading', url)
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = () => reject(new Error(`load ${url} fail`));
-    img.src = url;
-  });
-};
-
 function delay(time) {
   return new Promise(resolve =>{
     setTimeout(()=> resolve(),time )
   })
 }
 
-function loadImagesNice(cardData) {
-  let result = Promise.resolve()
-  let modifiedCardData = []
-  cardData.forEach( (card, index) => {
-    result = result.then(()=> delay(API_DELAY * index))
-    .then(()=>loadImage(card.image_uris[CARD_SIZE])
-    .then(img => {
-      modifiedCardData.push(Object.assign({img:img}, card))
-    }))
-  })
- return result.then(()=>modifiedCardData)
-}
 
-function parseText(){
-  let reg = /(\d+)\s*(.+)\n?\r?/g
-  let text = document.getElementById('text').value
-  let cards = []
-  while (m = reg.exec(text)) {
-    cards.push({
-      qty: m[1],
-      name: m[2],
+
+
+class Deck {
+  constructor(data, columns = 10, rows = 7) {
+    if(columns > 10 || rows > 7)
+      throw 'width or height too large'
+    this.data = data
+    this.rows = rows;
+    this.columns = columns;
+    this.fetchingData = Promise.resolve()
+    this.canvas = document.createElement("canvas");
+    this.context = this.canvas.getContext('2d')
+  }
+
+  static parseText(text){
+    let reg = /(\d+)\s*(.+)\n?\r?/g
+    let cards = []
+    let m
+    while ( m = reg.exec(text)) {
+      cards.push({
+        qty: m[1],
+        name: m[2],
+      })
+    }
+    return cards
+  }
+
+  static fetchCardData(cards){
+    let data = cards.map(card => ({name: card.name}))
+    return axios.post('https://api.scryfall.com//cards/collection',{
+      identifiers: data
+    }).then(response => {
+      if(response.data.not_found && response.data.not_found.length > 0)
+        throw 'could not find ' +  response.data.not_found.map(e => e.name).join(' ')
+      return response.data.data
     })
   }
-  return cards
-}
-function fetchCardData(cards){
-  let data = cards.map(card => ({name: card.name}))
-  return axios.post('https://api.scryfall.com//cards/collection',{
-    identifiers: data
-  }).then(response => {
-    if(response.data.not_found && response.data.not_found.length > 0)
-      throw 'could not find ' +  response.data.not_found.join(' ')
-    return response.data.data
-  })
-}
 
-function build(cardList){
-  cards = cardList.slice()
-  canvas = document.createElement("canvas");
-  context = canvas.getContext('2d');
-  canvas.style = "width: 100%;position: absolute;"
-  cardWidth = cardList[0].img.naturalWidth
-  cardHeight = cardList[0].img.naturalHeight
-  columns = 10;
-  rows = 7
-  canvas.width  = cardWidth*columns;
-  canvas.height = cardHeight*rows;
-  document.body.appendChild(canvas)
+  static loadImage(url) {
+    console.log('loading', url)
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(new Error(`load ${url} fail`));
+      img.src = url;
+    });
+  };
 
-  let current
-  for(var i = 0; i < rows; i++) {
-    for(var j = 0; j < columns; j++) {
-      if(i == rows-1 && j == columns-1)
-        break
-      if(current && current.qty >1){
-        current.qty--
-      }else{
-        current = cards.pop()
-      }
-      if(current && current.img && i!= columns-1){
-        context.drawImage(current.img, cardWidth*j, cardHeight*i)
+  loadImagesNice() {
+    let result = Promise.resolve()
+    let modifiedCardData = []
+    this.data.forEach( (card, index) => {
+      result = result
+      .then(()=> delay(API_DELAY * index))
+      .then(()=>Deck.loadImage(card.image_uris[CARD_SIZE]))
+      .then(img => card.img = img)
+    })
+   return result.then(()=> this)
+  }
+
+  render(){
+    let cards = this.data.slice().reverse()
+    let cardWidth = cards[0].img.naturalWidth
+    let cardHeight = cards[0].img.naturalHeight
+    this.canvas.width  = cardWidth*this.columns
+    this.canvas.height = cardHeight*this.rows
+
+    let current, qty
+    for(var i = 0; i < this.rows; i++) {
+      for(var j = 0; j < this.columns; j++) {
+        if(current && qty >0){
+          qty--
+        }else{
+          current = cards.pop()
+          if(!current)
+            return // we are out of cards
+          qty = current.qty - 1
+        }
+        if(!current.img)
+          throw 'we are missing an image? what?'
+          this.context.drawImage(current.img, cardWidth*j, cardHeight*i)
       }
     }
+    // context.drawImage('https://cdn1.mtggoldfish.com/images/gf/back.jpg', cardWidth*(columns-1), cardHeight*(rows-1))
   }
-  console.log(columns-1, rows-1)
-  // context.drawImage('https://cdn1.mtggoldfish.com/images/gf/back.jpg', cardWidth*(columns-1), cardHeight*(rows-1))
+
+  static create(unparsedText, width, height){
+    let deck = Deck.parseText(unparsedText)
+    return Deck.fetchCardData(deck).then(cardData => {
+      // add our new data to the deck
+      deck.forEach((card, index) => Object.assign(card, cardData[index]))
+      deck = new Deck(deck, width, height)
+
+      return deck.loadImagesNice(cardData)
+      .catch(error => alert(error, 'Error'));
+
+      return deck
+    })
+  }
 }
 
-
 function main() {
-  let cards = parseText()
-  fetchCardData(cards).then(cardData => {
-    // add our quantity to the cardData
-    cardData.forEach((card, index) => card.qty = cards[index].qty)
-
-    loadImagesNice(cardData).then(data => {
-      console.log(data)
-      build(data)
-    })
-
+  let text = document.getElementById('text').value
+  let container = document.getElementById('container')
+  Deck.create(text).then( deck => {
+    container.appendChild(deck.canvas)
+    deck.render()
   })
-  .catch(error => alert(error));
 }
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('text').placeholde = `Enter your decklist here in this format:
+  document.getElementById('text').placeholder = `Enter your decklist here in this format:
 4 Rafiq of the Many
 4 Fire // Ice
 1 Marit Lage Token
 1 Elspeth, Knight-Errant Emblem
 1 Back
 1 Test`
-  document.getElementById('text').value = `2 Island
-4 Manamorphose
-1 Mountain
-1 Noxious Revival
-3 Opt
-2 Past in Flames
-4 Pyretic Ritual
-2 Remand`
 
+  document.getElementById('text').value = '4 Rafiq of the Many'
   document.getElementById("submit").addEventListener("click", main)
 });
